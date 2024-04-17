a0c5a67e1beabf3c4448cd6ebf2a19b14e115bb6
Klif <sang.wicklife@student.moringaschool.com>
2024-04-17 12:48:43 +0300

    models boilerplate

--- server/tasks.py
+++ server/tasks.py
@@ -1,6 +1,8 @@
 from databaseconfig import db
 
 class Task(db.Model):
+    __tablename__ = 'tasks'
+    
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
     description = db.Column(db.Text)
